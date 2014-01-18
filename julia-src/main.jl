5c96a1f9fe417ac730761a85b902ad015a143c26
Jake Bolewski <jakebolewski@gmail.com>
2014-01-17 22:59:42 -0500

    little progress

--- julia-src/main.jl
+++ julia-src/main.jl
@@ -1,7 +1,12 @@
 using JSON
 
+#-------------
+# global state
+#--------------
 const PORT = int(ARGS[1])
 const CLID = int(ARGS[2])
+
+stopped::Bool = true
 
 function response_msg()
   client_msg = json({"name" => "Julia $(basename(pwd()))",
@@ -21,18 +26,66 @@ is_linenumber(ex::LineNumberNode) = true
 is_linenumber(ex::Expr) = is(ex.head, :line)
 is_linenumber(ex) = false
 
-function handle_pos(str, pos)
-  lines = split(str, "\n")
+get_linenumber(ex::Expr) = ex.args[1]
+get_linenumber(ex::LineNumberNode) = ex.line
+
+function find_loc(code::String)
+  nlines = length(split(str, "\n"))
+  codeblock = "begin;" * code * ";end"
+  expr = parse(codeblock)
+  nargs = length(expr.args)
+  res = {}
+  return expr
+  for (i, arg) in enumerate(expr.args)
+    if isa(arg, LineNumberNode) || arg.head == :line
+      continue
+    end
+    if is(arg.head, :function)
+      b = get_linenumber(arg.args[end].args[1])
+      e = i + 1 > nargs ? nlines : get_linenumber(expr.args[i + 1])
+        push!(res, (arg, (b - 1, e)))
+      continue
+    elseif (is(arg.head, :(=)) ||
+            is(arg.head, :->)) &&
+            is(arg.args[end].head, :block)
+      b = get_linenumber(arg.args[end].args[1])
+      e = i + 1 > nargs ? nlines : get_linenumber(expr.args[i + 1])
+      push!(res, (arg, (b, e)))
+      continue
+    elseif is(arg.head, :block) ||
+           is(arg.head, :let)
+       n = get_linenumber(arg.args[1])
+       push!(res, (arg, n - 1))
+    elseif is(arg.head, :type)
+       n = get_linenumber(arg.args[end].args[1])
+       push!(res, (arg, n - 1))
+    else
+      if i - 1 > 0 && i + 1 < length(expr.args)
+        push!(res, (arg, expr.args[i+1].line - 2))
+      elseif i + 1 > length(expr.args)
+        push!(res, (arg, nlines - 2))
+      else
+        push!(res, (arg, expr.args[i-1].line + 1))
+      end
+    end
+  end
+  return res
+end
+
+function to_form(loc, line, nlines)
+end
+
+function handle_pos(code, pos)
+  lines = split(code, "\n")
   nlines = length(lines)
   if nlines == 0
-    return (nothing, nothing)
+    return {(nothing, nothing)}
   end
-  expr = parseblock(str)
-  loc = find_loc(expr.args, pos["line"] + 1, nlines)
-  if loc != nothing
-    return to_form(lines, loc)
+  loc = find_loc(code, pos["line"] + 1, nlines)
+  if !isempty(loc)
+    return to_form(lines, loc, nlines)
   end
-  return (nothing, nothing)
+  return {(nothing, nothing)}
 end
 
 function parseall(str::String; greedy::Bool=true, raise=Bool=true)
@@ -56,13 +109,80 @@ function parseblock(str::String)
 end
 
 function clean_code(c)
-  return c #replace(c, r"(#.*coding.*)\n?", "\n")
+  return replace(c, r"(#.*coding.*)\n?", "\n")
+end
+
+function clean_trace(t)
+    return string(t)
 end
 
 function send_response(socket, id, cmd, args)
   data = json([id, cmd, args])
   write(socket, string(data, "\n"))
 end
+
+function handle_eval(data)
+    result = nothing
+    code = clean_code(msg["code"])
+    if haskey(msg, "meta")
+        loc = msg["meta"]
+    else
+        loc = {"start" => 1, "end" => 1}
+    end
+    to_exec = {}
+    if haskey(msg, "pos")
+        try
+            push!(to_exec, handle_pos(code, msg["pos"]))
+        catch err
+           return send_response(socket, id, "editor.eval.julia.exception",
+                                {"ex"   => clean_trace(err),
+                                 "meta" => {"start" => msg["pos"]["line"],
+                                            "end"   => msg["pos"]["line"]}})
+        end
+    else
+        try
+            exp = code
+            if haskey(msg, "meta")
+                exp = "\n"^msg["meta"]["start"] * code
+            end
+            to_exec = explode_code(exp)
+        catch
+            return send_response(socket, id, "editor.eval.julia.exception",
+                                 {"ex" => clean_trace(err),
+                                  "meta" => msg["meta"]})
+        end
+    end
+    if code == nothing || len(to_exec) == 0
+        return send_reponse(socket, id, "editor.eval.python.result", {})
+    end
+
+    for form in to_exec
+        expr, loc = form
+        if expr == nothing || loc == nothing
+            continue
+        end
+        try
+            res = eval(expr)
+            if res != nothing
+                send_response(socket, id, "editor.eval.julia.response",
+                             {"result" => utf8(res), "meta" => loc})
+            else
+                send_response(socket, id, "editor.eval.julia.success",
+                              {"meta" => loc})
+        catch err
+            try
+                send_response(socket, id, "editor.eval.julia.exception",
+                            {"ex" => clean_trace(err),
+                            "meta" => loc})
+                continue
+            catch err
+                println(err)
+            end
+        end
+    end
+end
+
+
 
 function eval_julia(socket, id, msg)
   try
